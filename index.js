fe4cbdc89864de5a84c8770ad8b5098e2d18c3e0
husseinahmed <hussenahmedkhalif122@gmail.com>
2024-05-27 17:28:01 +0300

    event-listening lab done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+
 function addingEventListener() {
+
+    const element = document.getElementById('button');
+    element.addEventListener('click', function() {
+        alert('Button was clicked!');
+    });
 }
+
+
+addingEventListener();
+
